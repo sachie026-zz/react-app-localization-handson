2d2d2ba967ab24fb69a72b3a6a09b3ae53102649
Sachin Jadhav <sachinjadhav@News-MacBook-Pro.local>
2019-12-02 20:15:26 +0530

    created project structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {IntlProvider} from "react-intl";
+import {FormattedMessage, FormattedHTMLMessage} from 'react-intl';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (   
+      <IntlProvider locale='en'>
+       <div className="App">
+         Hello!
+         <p>
+    <FormattedHTMLMessage id="app.text"
+                      defaultMessage="Hello!<br/>"
+                      description="Welcome header on app main page"
+                      values={{ what: 'react-intl' }}/>
+</p>
+<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+    <FormattedMessage id="app.learn-react-link"
+                      defaultMessage="Learn React"
+                      description="Link on react page"/>
+</a>
+       </div>
+    </IntlProvider>
   );
 }
 
